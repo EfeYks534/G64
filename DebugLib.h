a3898439181219d1b33bbb022b63ad1fd5850215
EfeYks534 <yefeyks@gmail.com>
2020-11-04 16:29:09 +0300

    Deleted debug library

--- DebugLib.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-void InitDebugger();
-
-void _StartProfile(char *name, char *file, int line);
-
-void EndProfile();
-
-#define StartProfile(name) _StartProfile(name, __FILE__, __LINE__)
