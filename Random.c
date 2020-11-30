ae8591c7a18f74b1dfef97255019e24993f9ff50
EfeYks534 <yefeyks@gmail.com>
2020-11-30 21:20:30 +0300

    Changed Random Number lib to use a high speed clock

--- Random.c
+++ Random.c
@@ -5,21 +5,104 @@
 #include <stdlib.h>
 #include <time.h>
 #include <sys/random.h>
+#include <pthread.h>
 
-uint64_t RNG()
+static uint8_t    _clock[16];
+static uint8_t       started;
+static uint8_t      priority;
+static uint8_t   manual_mode;
+static pthread_t      thread;
+
+static void *TickClock(void *_arg)
 {
-	uint64_t val = 0;
-	if(getrandom(&val, sizeof(val), 0) == -1) {
-		uint8_t *vals = (uint8_t*)&val;
-		for(int i = 0; i < 8; i++)
-			vals[i] = (uint8_t) rand();
+	uint64_t steps = 0;
+	while(1) {
+		for(int i = 0; i < 16; i++) {
+			steps++;
+			for(int j = 0; j < 16; j++) {
+				_clock[i] ++;
+				_clock[i] += (++_clock[j]) ^ (_clock[i]);
+				_clock[i] = (_clock[i] << (_clock[j] % 4)) |
+						(_clock[i] >> (8 - (_clock[j] % 4)));
+				if(priority > 0 && steps % 16 == 0) {
+					manual_mode = 1;
+					nanosleep(&(struct timespec){0, 256*priority}, NULL);
+					manual_mode = 0;
+				}
+			}
+		}
 	}
-	return val;
+
+	return NULL;
 }
 
-void FillRNG(void *ptr, size_t size)
+void SetClockPriority(uint8_t pri)
 {
-	if(getrandom(ptr, size, 0) == -1)
-		for(int i = 0; i < size; i++)
-			((uint8_t*)ptr)[i] = (uint8_t) rand();
+	switch(pri)
+	{
+	case G64_PRIORITY_MEGA: priority = 1; break;
+	case G64_PRIORITY_ULTRA: priority = 2; break;
+	case G64_PRIORITY_VHIGH: priority = 4; break;
+	case G64_PRIORITY_HIGH: priority = 8; break;
+	case G64_PRIORITY_MEDIUM: priority = 16; break;
+	case G64_PRIORITY_SMALL: priority = 32; break;
+	case G64_PRIORITY_LOW: priority = 64; break;
+	case G64_PRIORITY_VLOW: priority = 128; break;
+	default: priority = 16; break;
+	}
+}
+
+int StartClock()
+{
+	if(started) return -1;
+	SetClockPriority(G64_PRIORITY_MEDIUM);
+
+	int res1 = pthread_create(&thread, NULL, TickClock, NULL);
+	if(res1 != -1) started++;
+	return (res1 == -1) ? 0 : 1;
+}
+
+static uint64_t seed;
+
+static uint64_t ManualRNG()
+{
+	if(manual_mode) return 0;
+	struct timespec time = {0, 0};
+	clock_gettime(CLOCK_MONOTONIC_RAW, &time);
+	seed = (time.tv_sec+1) * time.tv_nsec;
+	return (seed << (time.tv_nsec % 36)) | (seed >> (time.tv_nsec % 24));
+}
+
+uint64_t RNG64()
+{
+	return (*(uint64_t*)_clock) ^ (*(uint64_t*)(&_clock[8])) ^ ManualRNG();
+}
+
+uint32_t RNG32()
+{
+	return (*(uint32_t*)(&_clock[4])) ^ (*(uint32_t*)(&_clock[0]))
+				^ (*(uint32_t*)(&_clock[8])) ^ ManualRNG();
+}
+
+uint16_t RNG16()
+{
+	return (*(uint16_t*)(&_clock[6])) ^ (*(uint16_t*)(&_clock[2])) ^ ManualRNG();
+}
+
+uint8_t RNG8()
+{
+	return _clock[0] ^ _clock[3] ^ ManualRNG();
+}
+
+static uint8_t parity_table[] = {1,0,0,1,0,1,1,0,0,1,1,0,1,0,0,1,0,1,1,0,1,0,0,1,1,0,0,1,0,1,1,0,0,1,1,0,1,0,0,1,1,0,0,1,0,1,1,0,1,0,0,1,0,1,1,0,0,1,1,0,1,0,0,1,0,1,1,0,1,0,0,1,1,0,0,1,0,1,1,0,1,0,0,1,0,1,1,0,0,1,1,0,1,0,0,1,1,0,0,1,0,1,1,0,0,1,1,0,1,0,0,1,0,1,1,0,1,0,0,1,1,0,0,1,0,1,1,0,0,1,1,0,1,0,0,1,1,0,0,1,0,1,1,0,1,0,0,1,0,1,1,0,0,1,1,0,1,0,0,1,1,0,0,1,0,1,1,0,0,1,1,0,1,0,0,1,0,1,1,0,1,0,0,1,1,0,0,1,0,1,1,0,1,0,0,1,0,1,1,0,0,1,1,0,1,0,0,1,0,1,1,0,1,0,0,1,1,0,0,1,0,1,1,0,0,1,1,0,1,0,0,1,1,0,0,1,0,1,1,0,1,0,0,1,0,1,1,0,0,1,1,0,1,0,0,1};
+
+uint8_t RNGBool()
+{
+	return parity_table[_clock[7]^(uint8_t)ManualRNG()];
+}
+
+void FillRNG(uint8_t *_ptr, size_t size)
+{
+	for(size_t i = 0; i < size; i++)
+		_ptr[i] = _clock[i % 16] ^ _clock[i % 8] ^ ManualRNG();
 }
--- RndLib.h
+++ RndLib.h
@@ -3,6 +3,27 @@
 #include <stdint.h>
 #include <stddef.h>
 
-uint64_t RNG(); // Get a random number
+#define G64_PRIORITY_MEGA   0x0
+#define G64_PRIORITY_ULTRA  0x1
+#define G64_PRIORITY_VHIGH  0x2
+#define G64_PRIORITY_HIGH   0x3
+#define G64_PRIORITY_MEDIUM 0x4
+#define G64_PRIORITY_SMALL  0x5
+#define G64_PRIORITY_LOW    0x6
+#define G64_PRIORITY_VLOW   0x7
 
-void FillRNG(void *ptr, size_t size); // Fill ptr with random numbers
+void SetClockPriority(uint8_t priority);
+
+int StartClock(); // Start high speed clock
+
+uint64_t RNG64(); // Get a 64 bit random number
+
+uint32_t RNG32(); // Get a 32 bit random number
+
+uint16_t RNG16(); // Get a 32 bit random number
+
+uint8_t RNG8(); // Get a 32 bit random number
+
+uint8_t RNGBool(); // Get a random boolean
+
+void FillRNG(uint8_t *ptr, size_t size); // Fill ptr with random numbers
